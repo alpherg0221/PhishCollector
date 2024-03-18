4f9e1ec069cfb58c3ffd40d6777be3fd6e0f8b38
alpherg0221 <alpherg0221@gmail.com>
2024-03-18 09:42:32 +0900

    Added collected page

--- client/src/collected/Collected.tsx
+++ client/src/collected/Collected.tsx
@@ -1,13 +1,78 @@
+import "./Collected.css";
+import {PhishInfo, useCollectedState} from "./CollectedState.ts";
+import {StackShim} from "@fluentui/react-migration-v8-v9";
+import {MdCalendarMonth, MdCrisisAlert, MdLink, MdRefresh, MdSailing, MdVerified} from "react-icons/md";
+import {
+  Body2,
+  Button, Divider,
+  LargeTitle,
+  Subtitle2, Tooltip,
+} from "@fluentui/react-components";
+import {FoodFishFilled} from "@fluentui/react-icons";
+
 function Collected() {
+  const state = useCollectedState();
+
+  const fetchData = async () => {
+    const server = "http:///www.az.lab.uec.ac.jp:8080/~ywatanabe/PhishCollector/api/collected/list";
+    const phishInfo: PhishInfo[] = await fetch(server, { mode: "cors" }).then(res => res.json());
+    state.update({ phishInfo: phishInfo.toSorted((a, b) => -a.date.localeCompare(b.date)) });
+  }
+
   return (
-    <div style={ {
-      minHeight: "100svh",
-      height: "auto",
-      width: "100%",
-      display: "grid",
-      justifyContent: "center",
-      alignItems: "center"
-    } }> 今後追加予定 </div>
+    <div className="centeringHorizontal">
+      <StackShim horizontalAlign="center" tokens={ { childrenGap: 48, padding: "48px 0px 48px 0px" } }>
+        { /*タイトル*/ }
+        <StackShim horizontal verticalAlign={ "center" } tokens={ { childrenGap: 12 } }>
+          <MdSailing style={ { width: 52, height: 52, color: "#38B48B" } }/>
+          <div>
+            <LargeTitle>Phish Collector </LargeTitle>
+            <Subtitle2>v1.1.2</Subtitle2>
+          </div>
+        </StackShim>
+
+        <Button
+          appearance={ "primary" }
+          children={ "Reload Data" }
+          icon={ <MdRefresh/> }
+          onClick={ async () => await fetchData() }
+          size={ "large" }
+        />
+
+        <StackShim tokens={ { childrenGap: 12 } }>
+          { state.phishInfo.map(info => (
+            <StackShim tokens={ { childrenGap: 12 } }>
+              <PhishInfoRow info={ info }/>
+              <Divider appearance={ "strong" }/>
+            </StackShim>
+          )) }
+        </StackShim>
+      </StackShim>
+    </div>
+  );
+}
+
+const PhishInfoRow = (props: { info: PhishInfo }) => {
+  return (
+    <StackShim horizontal verticalAlign={ "center" } tokens={ { childrenGap: 24, padding: "0px 48px 0px 48px" } }>
+      <StackShim horizontal verticalAlign={ "center" }>
+        <MdCalendarMonth size={ 18 } style={ { marginRight: 8 } }/>
+        <Body2>{ props.info.date }</Body2>
+      </StackShim>
+      <StackShim horizontal verticalAlign={ "center" }>
+        <MdLink size={ 18 } style={ { marginRight: 8 } }/>
+        <Tooltip content={ props.info.url } relationship={ "label" } withArrow>
+          <Body2 style={ { width: "35svw" } } truncate wrap={ false }>{ props.info.url }</Body2>
+        </Tooltip>
+      </StackShim>
+      <StackShim horizontal verticalAlign={ "center" }>
+        <MdCrisisAlert size={ 18 } style={ { marginRight: 8 } }/>
+        <Body2 style={ { width: 140 } }>{ props.info.target }</Body2>
+      </StackShim>
+      { props.info.gsb
+        ? <FoodFishFilled fontSize={ 18 } color={ "#FF0000" }/>
+        : <MdVerified size={ 18 } color={ "#00B379" }/> }
+    </StackShim>
   );
 }
 
