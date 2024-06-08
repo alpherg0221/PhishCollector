a61ffd3a41aecd1b7bc8506815f2855acaab32f8
alpherg0221 <alpherg0221@gmail.com>
2024-06-08 12:42:08 +0900

    Changed enum to class

--- client/src/home/HomeState.tsx
+++ client/src/home/HomeState.tsx
@@ -2,13 +2,49 @@ import {create} from "zustand";
 import {defaultServer, ServerStatus} from "../utils/server.tsx";
 import {JSX} from "react";
 import {GiFishBucket, GiFishing} from "react-icons/gi";
-import {MdError} from "react-icons/md";
+import {MdError, MdHelp, MdVerified} from "react-icons/md";
+import {FoodFishFilled} from "@fluentui/react-icons";
 
-export enum Warning {
-  Safe, Unknown, Phishing,
+
+class Warning {
+  public static readonly Safe = new Warning(0, "#00B379", <MdVerified/>);
+  public static readonly Unknown = new Warning(1, undefined, <MdHelp/>);
+  public static readonly Phishing = new Warning(2, "#FF0000", <FoodFishFilled/>);
+
+  public constructor(
+    private _order: number,
+    private _color: string | undefined,
+    private _icon: JSX.Element,
+  ) {
+  }
+
+  public get order() {
+    return this._order
+  }
+
+  public get color() {
+    return this._color;
+  }
+
+  public get icon() {
+    return this._icon;
+  }
+
+  public get next() {
+    switch (this) {
+      case Warning.Safe:
+        return Warning.Unknown;
+      case Warning.Unknown:
+        return Warning.Phishing;
+      case Warning.Phishing:
+        return Warning.Safe;
+      default:
+        return Warning.Unknown;
+    }
+  }
 }
 
-export class CollectStatus {
+class CollectStatus {
   public static readonly NotCollected = new CollectStatus("NotCollected", <GiFishing/>, "Crawl Phishing Page");
   public static readonly Collecting = new CollectStatus("Collecting", undefined, "Collecting");
   public static readonly Collected = new CollectStatus("Collected", <GiFishBucket color="#00B379"/>, "Collected");
@@ -34,24 +70,14 @@ export class CollectStatus {
   }
 }
 
-export const warningOrder = Object.values(Warning);
-
-export const warningNextTo = (warning: Warning) => {
-  switch (warning) {
-    case Warning.Safe:
-      return Warning.Unknown;
-    case Warning.Unknown:
-      return Warning.Phishing;
-    case Warning.Phishing:
-      return Warning.Safe;
-  }
+class SortBy {
+  public static readonly Url = new SortBy();
+  public static readonly Target = new SortBy();
+  public static readonly GSB = new SortBy();
+  public static readonly Browser = new SortBy();
 }
 
-export enum SortBy {
-  Url, Target, GSB, Browser,
-}
-
-export type UrlInfo = {
+type UrlInfo = {
   url: string,
   target: string,
   warning: {
@@ -74,7 +100,7 @@ type HomeAction = {
   reset: () => void;
 }
 
-export const defaultHomeState: HomeState = {
+const defaultHomeState: HomeState = {
   urlInfo: [{
     url: "",
     target: "",
@@ -87,8 +113,19 @@ export const defaultHomeState: HomeState = {
   serverStatus: ServerStatus.LOADING,
 }
 
-export const useHomeState = create<HomeState & HomeAction>((set) => ({
+const useHomeState = create<HomeState & HomeAction>((set) => ({
   ...defaultHomeState,
   update: (newState) => set(newState),
   reset: () => set(defaultHomeState),
 }));
+
+export {
+  Warning,
+  CollectStatus,
+  SortBy,
+  defaultHomeState,
+  useHomeState,
+}
+export type {
+  UrlInfo,
+}
